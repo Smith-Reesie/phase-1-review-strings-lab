174a50ec9ee4348be0460bb1ec82e28ac35abe27
Maurice Smith <Xmaurice.smith@gmail.com>
2021-10-12 17:04:09 -0400

    commit tree 69961c1971a3be388f89ad1e14f15850a568ae96

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Felix Zheng';
+
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, FELIX ZHENG!`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
